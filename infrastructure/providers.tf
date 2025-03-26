b9467c3b75693d13b1edd09f614988f2577678e0
Heeyai Chen <chenkonsam@gmail.com>
2025-03-26 20:16:56 +0530

    - remove backend remote state

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -5,15 +5,7 @@ terraform {
       version = "4.23.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstateqrcode123"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
 }
-
 provider "azurerm" {
   features {}
 }
